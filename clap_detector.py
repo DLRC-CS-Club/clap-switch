f75a6c937dfd3472502c50c4a2a68bbf4a307a99
Ansh Tiwatne <ansh.tiwatne@gmail.com>
2023-03-19 12:13:26 +0530

    Add files via upload

---
+++ clap_detector.py
@@ -0,0 +1,28 @@
+#!/usr/bin/python3
+
+from gpiozero import LED
+import piclap
+
+light = LED(17)
+
+class Config(piclap.Settings):
+    def __init__(self):
+        super().__init__()
+        self.chunk_size = 512
+        self.wait = 0.5
+        self.method.value = 600
+
+    def on2Claps(self):
+        light.toggle()
+        print(f"Light Toggled at GPIO: {light.pin}")
+
+    def on4Claps(self):
+        pass
+
+class Listner(piclap.Listener):
+    def confirm(self):
+        pass
+
+
+listener = piclap.Listener(Config(), calibrate=False)
+listener.start()
